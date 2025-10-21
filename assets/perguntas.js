c1c8875f50ab5c24f38d00b7c91b23e6ffd461a7
GABRIEL DA SILVA LEMES <lemes.gabriel07@escola.pr.gov.br>
2025-10-21 00:17:27 +0000

    078

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,14 +1,16 @@
-criarCartao(
-      'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação'
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
-criarCartao(
+
+criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
-criarCartao(
+
+criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
