77ec9b8dce5c06e7c8604369c1a8b3b33eaa9bc0
GABRIEL DA SILVA LEMES <lemes.gabriel07@escola.pr.gov.br>
2025-10-29 00:50:33 +0000

    50

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,70 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'matematica',
+    'como funciona potenciação ',
+    'representa multiplicações repetidas, fazendo com que os números cresçam rapidamente'
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'português',
+    'quais são os quatro porques?',
+    'por que (separado e sem acento), por quê (separado e com acento), porque (junto e sem acento) e porquê (junto e com acento)'
+)
+
+criaCartao(
+    'sociologia',
+    'a sociologia e a ciência que estuda?',
+    'ciência humana que estuda a sociedade, as relações sociais, as interações e a cultura de forma científica e crítica'
+)
+
+criaCartao(
+    'resolução de problemas',
+    'qual tipo de processo envolve a resolução de problemas?',
+    'é um processo sistemático para identificar e resolver situações indesejadas, usando métodos ordenados para chegar a uma solução'
+)
+
+criaCartao(
+    'historia',
+    'o que a historia estuda?',
+    'ciência que estuda o passado da humanidade, investigando e interpretando ações humanas, eventos, culturas e sociedades ao longo do tempo a partir de evidências como documentos, artefatos e registros'
+)
+criaCartao(
+    'projeto de vida',
+    'projeto de vida ajuda no que?',
+    'Um projeto de vida ajuda a planejar o futuro, tomar decisões mais conscientes sobre carreira e estudos, e a desenvolver o autoconhecimento e habilidades socioemocionais. Ele fornece um senso de propósito, orienta as escolhas do dia a dia e aumenta a motivação, ajudando a lidar com a auto-responsabilidade e a alcançar objetivos pessoais e profissionais. '
+)
+
+criaCartao(
+    'química',
+    'o símbolo químico de k representa o que em química?',
+    'O símbolo "K" na química representa o potássio. O símbolo vem do nome latino do elemento, kalium. O potássio é um metal alcalino com número atômico 19. '
+)
+
+criaCartao(
+    'biologia',
+    'qual a primeira lei de mendel?',
+    'A primeira lei de Mendel, também conhecida como Lei da Segregação dos Fatores, estabelece que cada característica hereditária é determinada por um par de fatores (genes) que se separam durante a formação dos gametas. Dessa forma, cada gameta recebe apenas um fator do par, e o indivíduo descendente herda um fator de cada genitor. '
+)
+
+criaCartao(
+    'física',
+    'quais são os quatro ramos de estudo da física?',
+    'Óptica, Acústica, Eletromagnetismo e Mecânica Clássica.'
+)
+criaCartao(
+    'educação financeira',
+    'o que é o método 50 30 20?',
+    'O método 50/30/20 é uma regra de orçamento que divide a renda mensal líquida (após impostos) em três categorias: 50% para necessidades essenciais (como aluguel, alimentação e transporte), 30% para gastos com desejos (como lazer, hobbies e restaurantes) e 20% para prioridades financeiras (como poupança, investimentos e quitação de dívidas). Ele ajuda a organizar as finanças pessoais de forma prática, buscando equilibrar gastos obrigatórios, prazeres e planejamento futuro. '
+)
+
+criaCartao(
+    'biologia I',
+    'para que serve a biotecnologia?',
+    'é o uso de organismos vivos, partes deles ou seus processos biológicos para desenvolver produtos e tecnologias em áreas como saúde, agricultura, meio ambiente e indústria'
 )
